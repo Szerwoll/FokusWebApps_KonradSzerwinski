a351cf98b1a6398d1de5f5e5123394967bfa6786
Szerwoll <szerwol@gmail.com>
2018-11-18 00:23:13 +0100

    Phone number validation

--- front/src/app/widget/widget.component.ts
+++ front/src/app/widget/widget.component.ts
@@ -1,30 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { CallService } from '../call.service';
-import { stringify } from '@angular/compiler/src/util';
 import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-widget',
   templateUrl: './widget.component.html',
-  styleUrls: ['./widget.component.css'],
+  styleUrls: ['./widget.component.css']
 })
+
 export class WidgetComponent implements OnInit {
   number: string;
-  validator = /(^[0-9]{9}$)/;
+  validator = /^(?:\(?\+?48)?(?:[-\.\(\)\s\+\48]*(\d)){9}\)?$/;
   exampleText = 'Fokus Web App';
-
-  // constructor(private callService: CallService) {}
+  validNumber: string;
 
   constructor(private router: Router) { }
 
   ngOnInit() {
-    this.validator = /(^[0-9]{9}$)/;
+    this.validator = /^(?:\(?\+?48)?(?:[-\.\(\)\s\+\48]*(\d)){9}\)?$/;
   }
 
   call() {
     if (this.isValidNumber()) {
-      this.router.navigate(['/ringing/' + this.number]);
+      this.validNumber = this.number.replace(/\D+/g, '');
+      this.validNumber = this.validNumber.substr(this.validNumber.length - 9);
+      console.log(this.validNumber);
+      this.router.navigate(['/ringing/' + this.validNumber]);
     } else {
       // tslint:disable-next-line:no-console
       console.info('Numer niepoprawny');
