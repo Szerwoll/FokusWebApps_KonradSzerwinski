da1456e11676c972ee49229a87a36a49cb299a2e
Szerwoll <szerwol@gmail.com>
2018-11-18 00:24:43 +0100

    Added return to first page after call finish

--- front/src/app/finished/finished.component.ts
+++ front/src/app/finished/finished.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CallService } from './../call.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-finished',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinishedComponent implements OnInit {
 
-  constructor() { }
+  interval: any;
+
+  constructor(private router: Router, private callService: CallService) { }
 
   ngOnInit() {
+    this.interval = setInterval(() => {
+      this.router.navigate(['/']);
+      clearInterval(this.interval);
+
+    }, 3000);
   }
 
 }
